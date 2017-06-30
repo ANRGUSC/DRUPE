a33d0c1ac0991c833b6ec796054fcfef9f097b9a
jiatongw <codewjt@gmail.com>
2017-06-30 00:10:52 -0700

    first release

---
+++ Resource_Profiler/Resource_Profiler_server/install_package.py
@@ -0,0 +1,30 @@
+'''
+ * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ *     contributor: Jiatong Wang
+ *     Read license file in main directory for more details
+'''
+
+import os
+import site
+
+
+try:
+    import psutil
+except ImportError:
+    print "no lib psutil"
+    import pip
+    cmd = "sudo pip2 install psutil"
+    print "Requests package is missing\nPlease enter root password to install required package"
+    os.system(cmd)
+    reload(site)
+
+
+try:
+    import flask
+except ImportError:
+    print "no lib flask"
+    import pip
+    cmd = "sudo pip2 install flask"
+    print "Requests package is missing\nPlease enter root password to install required package"
+    os.system(cmd)
+    reload(site)
