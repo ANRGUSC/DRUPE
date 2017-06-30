edc0b9e2e8607e2167737018a968d9b3f4c780b2
jiatongw <codewjt@gmail.com>
2017-06-30 12:02:44 -0700

    none container version for Resource Profiler

---
+++ DCP/Resource_Profiler/Resource_Profiler_control/install_package.py
@@ -0,0 +1,19 @@
+'''
+ * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ *     contributor: Jiatong Wang
+ *     Read license file in main directory for more details
+'''
+
+import os
+import site
+
+
+try:
+    import requests
+except ImportError:
+    print "no lib requests"
+    import pip
+    cmd = "sudo pip2 install requests"
+    print "Requests package is missing\nPlease enter root password to install required package"
+    os.system(cmd)
+    reload(site)
