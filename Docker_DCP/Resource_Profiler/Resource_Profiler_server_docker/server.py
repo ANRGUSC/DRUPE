a001d1be47967b1d4e800e0c5b44e9ed111c4e08
jiatongw <codewjt@gmail.com>
2017-06-30 17:23:18 -0700

    modify notes

--- Docker_DCP/Resource_Profiler/Resource_Profiler_server_docker/server.py
+++ Docker_DCP/Resource_Profiler/Resource_Profiler_server_docker/server.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 @app.route('/') #web route url
 def performance():
     response = {} #create a response json object
-    response["memory"]=psutil.virtual_memory().percent #Return statistics about system memory usage
-    response["cpu"]=psutil.cpu_percent() #Return a float representing the current system-wide CPU utilization as a percentage.
-    return json.dumps(response) #output performance information by json format
+    response["memory"]=psutil.virtual_memory().percent 
+    response["cpu"]=psutil.cpu_percent() 
+    return json.dumps(response) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') #run this web application on 0.0.0.0 and default port is 5000
